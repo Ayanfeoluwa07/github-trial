91195123d72e630bb20ab89304453b1fd445e4b1
Ayanfeoluwa07 <howokoniranheryomi07@gmail.com>
2023-11-09 21:27:11 +0100

    first commit

---
+++ JS/index.js
@@ -0,0 +1,15 @@
+//console.log();
+function alert(){
+    prompt('Will you sign up')
+}
+
+let temperature = 20
+//console.log(temperature);
+// let name = 'kopiko'
+// console.log(name);
+//let student = false;
+// console.log(faruq);
+// let faydat = null
+// console.log(faydat);
+
+const numerals =[1, 'Ayanfe', true]
